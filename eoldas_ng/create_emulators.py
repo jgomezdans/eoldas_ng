0bacb69cbed6fd61aa209567ab476a97fdecded8
José Gómez-Dans <jgomezdans@gmail.com>
2013-11-11 18:45:31 +0000

    Emulation scheme seems to work. Only does prosail at the moment, but that's enough to test things.
    There's a distinct need to have per-band emulators, as these are likely to be faster, but might have some other issues to do with non-linearities etc. Worth learning a bit about that

--- eoldas_ng/create_emulators.py
+++ eoldas_ng/create_emulators.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+sys.path.append ("/home/ucfajlg/Data/python/gp_emulator")
 import pickle
 import json
 import csv
@@ -12,7 +13,7 @@ import scipy.stats as ss
 
 from lhd import lhd
 import prosail
-
+from multivariate_gp import MultivariateEmulator
 
 
 class PersistentDict(dict):
@@ -89,37 +90,58 @@ class PersistentDict(dict):
 
 
 
-if __name__ == '__main__':
-    import random
+#if __name__ == '__main__':
+    #import random
 
-    # Make and use a persistent dictionary
-    with PersistentDict('/tmp/demo.json', 'c', format='json') as d:
-        print(d, 'start')
-        d['abc'] = '123'
-        d['rand'] = random.randrange(10000)
-        print(d, 'updated')
+    ## Make and use a persistent dictionary
+    #with PersistentDict('/tmp/demo.json', 'c', format='json') as d:
+        #print(d, 'start')
+        #d['abc'] = '123'
+        #d['rand'] = random.randrange(10000)
+        #print(d, 'updated')
 
-    # Show what the file looks like on disk
-    with open('/tmp/demo.json', 'rb') as f:
-        print(f.read())
+    ## Show what the file looks like on disk
+    #with open('/tmp/demo.json', 'rb') as f:
+        #print(f.read())
 
+def fixnan(x):
+  '''
+  the RT model sometimes fails so we interpolate over nan
 
+  This method replaces the nans in the vector x by their interpolated values
+  '''
+  for i in xrange(x.shape[0]):
+    sample = x[i]
+    ww = np.where(np.isnan(sample))
+    nww = np.where(~np.isnan(sample))
+    sample[ww] = np.interp(ww[0],nww[0],sample[nww])
+    x[i] = sample
+  return x
 
-def create_emulators ( state, v_size=200, n_size=100, vzax = np.arange ( 5, 60, 5 ), \
-       szax = np.arange ( 5, 60, 5 ), raax = np.arange (-180, 180, 30 )  ):
-
+def create_emulators ( state, fnames, v_size=200, n_size=200, angles=None, \
+       vzax = np.arange ( 5, 60, 5 ), szax = np.arange ( 5, 60, 5 ), \
+       raax = np.arange (-180, 180, 30 )  ):
+    """
+    This function produces the sampling of parameter space based on the
+    state object that it receives. It then goes to generate the forward
+    models required, as well as a random sampling to validate against.
+    
+    TODO completeme!!!!!    
+    """
     distributions = []
     for param, minval in state.parameter_min.iteritems():
         if param != "lidfb":
             maxval = state.parameter_max[param]
-            distributions.append ( ss.uniform( loc=minval, scale=(maxval-minval)) )
+            distributions.append ( ss.uniform( loc=minval, \
+                scale=(maxval-minval)) )
     samples = lhd ( dist=distributions, size=n_size )
     # We need to add the lidfb parameter to the array
-    x = np.zeros((100, 12)
+    x = np.zeros(( n_size, 11))
     x[:, :9] = samples[:,:9]
     x[:, -2:] = samples[:, -2:]
-    angles = np.array ( [angle \
-        for angle in itertools.product ( szax, vzax, raax )] )
+    if angles is None:
+        angles = np.array ( [angle \
+            for angle in itertools.product ( szax, vzax, raax )] )
     validate = []
     for d in distributions:
         validate.append ( d.rvs( v_size ) )
@@ -129,11 +151,125 @@ def create_emulators ( state, v_size=200, n_size=100, vzax = np.arange ( 5, 60, 
     V[:, 10:12] = validate[:, -2:]
     V[:, -1] = 2
     S = np.zeros ((n_size, 16))
-    S[:, :12] = X
+    S[:, :9] = samples[:, :9]
+    S[:, 10:12] = samples[:, -2:]
     S[:, -1] = 2
 
-    for angle in angles:
+    gps = []
+    for i, angle in enumerate ( angles ):
         S[:, 12:15] = angle 
         V[:, 12:15] = angle
         train_brf = np.array ( [ prosail.run_prosail ( *s ) for s in S ] )
         validate_brf = np.array ( [ prosail.run_prosail ( *s ) for s in V ] )
+        train_brf = fixnan ( train_brf )
+        validate_brf = fixnan ( validate_brf )
+        emu, rmse = do_mv_emulation ( x, validate, train_brf, validate_brf )
+        print "Dumping to %s (RMSE:%g)" % ( fnames[i], rmse )
+        emu.dump_emulator ( fnames[i] )
+        gps.append ( emu )
+        
+    return gps
+
+def do_band_emulation ( ):
+    """
+    TODO: not even there yet!!!
+    """
+    pass
+
+def do_mv_emulation ( xtrain, xvalidate, train_brf, validate_brf ):
+    N = xvalidate.shape[0]
+    emu = MultivariateEmulator (y=xtrain, X=train_brf )
+    rmse = np.sqrt(np.mean([(validate_brf[i] - \
+         emu.predict ( np.atleast_2d(xvalidate[i,:]))[0])**2 \
+         for i in xrange(N)]))
+    return emu, rmse
+
+
+
+if __name__ == "__main__":
+    from collections import OrderedDict
+    from operators import *
+    FIXED = 1
+    CONSTANT = 2
+    VARIABLE = 3
+    # Create the state
+    # First, define the state configuration dictionary
+    
+    state_config = OrderedDict ()
+    
+    state_config['n'] = CONSTANT
+    state_config['cab'] = VARIABLE
+    state_config['car'] = CONSTANT
+    state_config['cbrown'] = VARIABLE
+    state_config['cw'] = VARIABLE
+    state_config['cm'] = VARIABLE
+    state_config['lai'] = VARIABLE
+    state_config['ala'] = CONSTANT
+    state_config['lidfb'] = FIXED
+    state_config['bsoil'] = CONSTANT
+    state_config['psoil'] = VARIABLE
+    state_config['hspot'] = CONSTANT
+    
+    
+    
+    
+    # Now define the default values
+    default_par = OrderedDict ()
+    default_par['n'] = 1.5
+    default_par['cab'] = 40.
+    default_par['car'] = 10.
+    default_par['cbrown'] = 0.01
+    default_par['cw'] = 0.018 # Say?
+    default_par['cm'] = 0.0065 # Say?
+    default_par['lai'] = 2
+    default_par['ala'] = 45.
+    default_par['lidfb'] = 0.
+    default_par['bsoil'] = 1.
+    default_par['psoil'] = 0.1
+    default_par['hspot'] = 0.01
+    
+    
+    # Define boundaries
+    #parameter_names = [ 'bsoil', 'cbrown', 'hspot', 'n', \
+    #    'psoil', 'ala', 'lidfb', 'cab', 'car', 'cm', 'cw', 'lai' ]
+    parameter_min = OrderedDict()
+    parameter_max = OrderedDict()
+    
+    min_vals = [ 0.8, 0.2, 0.0, 0.0, 0.0043, 0.0017, 0.001, 0., 0., 0., 0., 0.001]
+    max_vals = [2.5, 77., 25., 1., 0.0713, 0.0331, 8., 90., 0., 2., 8., 0.999]
+
+
+        
+        
+    for i, param in enumerate ( state_config.keys() ):
+        parameter_min[param] = min_vals[i]
+        parameter_max[param] = max_vals[i]
+    # Define the state grid. In time in this case
+    state_grid = np.arange ( 1, 366 )
+    # Define parameter transformations
+    transformations = {
+        'lai': lambda x: np.exp ( -x/2. ), \
+        'cab': lambda x: np.exp ( -x/100. ), \
+        'car': lambda x: np.exp ( -x/100. ), \
+        'cw': lambda x: np.exp ( -50.*x ), \
+        'cm': lambda x: np.exp ( -100.*x ), \
+        'ala': lambda x: x/90. }
+    inv_transformations = {
+        'lai': lambda x: -2*np.log ( x ), \
+        'cab': lambda x: -100*np.log ( x ), \
+        'car': lambda x: -100*np.log( x ), \
+        'cw': lambda x: (-1/50.)*np.log ( x ), \
+        'cm': lambda x: (-1/100.)*np.log ( x ), \
+        'ala': lambda x: 90.*x }
+    
+    # Define the state
+    # L'etat, c'est moi
+    state = State ( state_config, state_grid, default_par, \
+        parameter_min, parameter_max )
+    # Set the transformations
+    state.set_transformations ( transformations, inv_transformations )
+    vza = [30.]
+    sza = [0.]
+    raa = [40.]
+    fnames = [ "/tmp/vza_30_sza_0_raa_40" ]
+    gps = create_emulators ( state, fnames, angles= [[30, 0, 40]])
