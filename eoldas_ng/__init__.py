1a1131202c977cbac06037627f177ec651c7c0c4
José Gómez-Dans <jgomezdans@gmail.com>
2013-12-11 16:47:22 +0000

    Added an __init__ file to cleanly impor things

--- eoldas_ng/__init__.py
+++ eoldas_ng/__init__.py
@@ -1,1 +1,9 @@
-from operators import *
+from state import State
+
+from operators import Prior, TemporalSmoother, SpatialSmoother
+from operators import ObservationOperator, ObservationOperatorTimeSeriesGP
+from operators import ObservationOperatorImageGP
+
+from two_stream import ObservationOperatorTwoStream
+from two_stream import select_emulator, create_emulators 
+
