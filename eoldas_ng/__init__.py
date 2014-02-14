3372d78eecfac888ccf053021c13acfdb12c8a35
José Gómez-Dans <jgomezdans@gmail.com>
2014-02-14 18:49:06 +0000

    Some minor typos etc

--- eoldas_ng/__init__.py
+++ eoldas_ng/__init__.py
@@ -3,7 +3,8 @@ from state import State
 from operators import Prior, TemporalSmoother, SpatialSmoother
 from operators import ObservationOperator, ObservationOperatorTimeSeriesGP
 from operators import ObservationOperatorImageGP
+from operators import FIXED, CONSTANT, VARIABLE
 
-from two_stream import ObservationOperatorTwoStream
-from two_stream import select_emulator, create_emulators 
+#from two_stream import ObservationOperatorTwoStream
+#from two_stream import select_emulator, create_emulators 
 
