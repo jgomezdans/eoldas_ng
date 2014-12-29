c096ac45ba0d96ce0d97ef98d081fddea909ec3d
J Gomez-Dans <j.gomez-dans@ucl.ac.uk>
2014-12-29 16:11:14 +0000

    First commit on the helpers

---
+++ eoldas_ng/eoldas_helpers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+eoldas_ng helper functions/classes
+=======================================
+
+Here we provide  a set of helper functions/classes that provide some often
+required functionality. None of this is *actually* required to run the system,
+but it should make the library more user friendly.
+
+"""
+
+__author__  = "J Gomez-Dans"
+__version__ = "1.0 (29.12.2014)"
+__email__   = "j.gomez-dans@ucl.ac.uk"
+
+from collections import OrderedDict
+from operators import *
